0bf3316204abee943df0449b4cabb0d8d5bdf3a1
Jonathan Matsuda <jonathanmatsuda3@gmail.com>
2021-10-12 18:16:35 -0700

    Created an array for the items called productsOrdered

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -5,12 +5,15 @@ var orderHistory = [
     shipTo: 'JS Masher',
     orderNumber: 12345,
     deliveryConfirmation: 'Delivered Aug 8, 2020',
-    productInfo: {
-      title: 'JavaScript for impatient programmers',
-      author: 'Rauschmayer, Dr. Axel',
-      returnWindow: 'Return window closed on Sep 7, 2020',
-      price: 31.55
-    }
+    productsOrdered: [
+      {
+        title: 'JavaScript for impatient programmers',
+        author: 'Rauschmayer, Dr. Axel',
+        returnWindow: 'Return window closed on Sep 7, 2020',
+        price: 31.55,
+        imageUrl: 'path to image'
+      }
+    ]
   },
   {
     orderPlaced: 'July 19,2020',
@@ -18,12 +21,15 @@ var orderHistory = [
     shipTo: 'JS Masher',
     orderNumber: 23456,
     deliveryConfirmation: 'Delivered Jul 20, 2020. Your package was delivered. It was handed directly to resident.',
-    productInfo: {
-      title: 'The Timeless Way of Building',
-      author: 'Alexander, Christopher',
-      returnWindow: 'Return window closed on Aug 19, 2020',
-      price: 41.33
-    }
+    productsOrdered: [
+      {
+        title: 'The Timeless Way of Building',
+        author: 'Alexander, Christopher',
+        returnWindow: 'Return window closed on Aug 19, 2020',
+        price: 41.33,
+        imageUrl: 'path to image'
+      }
+    ]
   },
   {
     orderPlaced: 'July 4, 2020',
@@ -31,11 +37,14 @@ var orderHistory = [
     shipTo: 'JS Masher',
     orderNumber: 34567,
     deliveryConfirmation: 'Delivered July 7, 2020. Your package was delivered. It was handed directly to resident.',
-    productInfo: {
-      title: 'Gamecube Controller Adapter',
-      returnWindow: 'Return window closed on Aug 5, 2020',
-      price: 15.98
-    }
+    productsOrdered: [
+      {
+        title: 'Gamecube Controller Adapter',
+        returnWindow: 'Return window closed on Aug 5, 2020',
+        price: 15.98,
+        imageUrl: 'path to image'
+      }
+    ]
   },
   {
     orderPlaced: 'July 3, 2020',
@@ -43,17 +52,21 @@ var orderHistory = [
     shipTo: 'JS Masher',
     orderNumber: 45678,
     deliveryConfirmation: 'Deliverd Jul 5, 2020',
-    productInfo: {
-      title: 'Gamecube Controller',
-      returnWindow: 'Return window closed on Aug 4, 2020',
-      price: 94.95
-    },
-    productInfo2: {
-      title: 'The Art of Sql',
-      author: 'Faroult, Stephanie',
-      returnWindow: 'Return window closed on Aug 4, 2020',
-      price: 33.99
-    }
+    productsOrdered: [
+      {
+        title: 'Gamecube Controller',
+        returnWindow: 'Return window closed on Aug 4, 2020',
+        price: 94.95,
+        imageUrl: 'path to image'
+      },
+      {
+        title: 'The Art of Sql',
+        author: 'Faroult, Stephanie',
+        returnWindow: 'Return window closed on Aug 4, 2020',
+        price: 33.99,
+        imageUrl: 'path to image'
+      }
+    ]
   }
 ];
 
