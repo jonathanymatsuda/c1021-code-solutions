89f8443b29a4028345d6dc6d8ccebe55c315eb8b
Jonathan Matsuda <jonathanmatsuda3@gmail.com>
2021-10-20 00:05:17 -0700

    Fixed condition and bracket formatting

--- dom-manipulation/main.js
+++ dom-manipulation/main.js
@@ -6,19 +6,17 @@ function buttonChange() {
   $clickCount.textContent = 'Clicks: ' + numberOfClicks;
   if (numberOfClicks < 4) {
     $hotButton.className = 'hot-button cold';
-  } else if
-  (numberOfClicks < 7) {
+  } else if (numberOfClicks < 7) {
     $hotButton.className = 'hot-button cool';
-  } else if
-  (numberOfClicks < 10) {
+  } else if (numberOfClicks < 10) {
     $hotButton.className = 'hot-button tepid';
-  } else if
-  (numberOfClicks < 13) {
+  } else if (numberOfClicks < 13) {
     $hotButton.className = 'hot-button warm';
-  } else if
-  (numberOfClicks < 16) {
+  } else if (numberOfClicks < 16) {
     $hotButton.className = 'hot-button hot';
-  } else { $hotButton.className = 'hot-button nuclear'; }
+  } else {
+    $hotButton.className = 'hot-button nuclear';
+  }
 }
 
 $hotButton.addEventListener('click', buttonChange);
