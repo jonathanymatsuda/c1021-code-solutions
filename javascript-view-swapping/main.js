0e94a989ec0a3502262ba93a38df9a1fe57e8ce4
Jonathan Matsuda <jonathanmatsuda3@gmail.com>
2021-10-22 00:50:19 -0700

    In progress

---
+++ javascript-view-swapping/main.js
@@ -0,0 +1,13 @@
+// var $tabContainer = document.querySelector('.tab-container');
+// var $tab = document.querySelectorAll('.tab');
+// var $view = document.querySelectorAll('.view');
+
+// $tabContainer.addEventListener('click', function (event) {
+//   if (event.target.matches('.tab')) {
+//     console.log
+
+// function tabClick()
+// for (var i = 0; i < $tab.length; i++) {
+//   $tab[i] === event.target;
+//   $tab[i].className = 'tab active';
+// }
