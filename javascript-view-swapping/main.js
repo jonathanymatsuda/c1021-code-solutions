636ce572385eecd11af46de01b49434943ed5ed5
Jonathan Matsuda <jonathanmatsuda3@gmail.com>
2021-10-23 01:26:34 -0700

    Complete javascript-view-swapping exercise

--- javascript-view-swapping/main.js
+++ javascript-view-swapping/main.js
@@ -1,16 +1,24 @@
 var $tabContainer = document.querySelector('.tab-container');
 var $tab = document.querySelectorAll('.tab');
-// var $view = document.querySelectorAll('.view');
-
-$tabContainer.addEventListener('click', function (event) {
+var $view = document.querySelectorAll('.view');
+function viewSwapping(event) {
   if (event.target.matches('.tab')) {
-    for (var i = 0; i < $tab.length; i++) {
-      if ($tab[i] === event.target) {
-        $tab[i].className = 'tab active';
+    for (var tabIndex = 0; tabIndex < $tab.length; tabIndex++) {
+      if ($tab[tabIndex] === event.target) {
+        $tab[tabIndex].className = 'tab active';
       } else {
-        $tab[i].className = 'tab';
+        $tab[tabIndex].className = 'tab';
+      }
+    }
+    var $dataView = event.target.getAttribute('data-view');
+    for (var viewIndex = 0; viewIndex < $view.length; viewIndex++) {
+      if ($view[viewIndex].getAttribute('data-view') === $dataView) {
+        $view[viewIndex].className = 'view';
+      } else {
+        $view[viewIndex].className = 'view hidden';
       }
     }
   }
 }
-);
+
+$tabContainer.addEventListener('click', viewSwapping);
