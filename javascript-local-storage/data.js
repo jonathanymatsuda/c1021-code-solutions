63181190a3bc802da57c6d625b6491eade4aa6da
Jonathan Matsuda <jonathanmatsuda3@gmail.com>
2021-10-25 11:44:22 -0700

    Null replaced

--- javascript-local-storage/data.js
+++ javascript-local-storage/data.js
@@ -2,7 +2,7 @@
 
 var todos = [];
 var previousTodosJSON = localStorage.getItem('javascript-local-storage');
-if (previousTodosJSON !== 'null') {
+if (previousTodosJSON !== null) {
   todos = JSON.parse(previousTodosJSON);
 }
 
