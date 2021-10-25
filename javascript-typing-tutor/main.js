b43b8713e7a40d70089118d5c566c26be4299bc6
Jonathan Matsuda <jonathanmatsuda3@gmail.com>
2021-10-25 01:08:49 -0700

    Completed javascript-typing-tutor exercise

---
+++ javascript-typing-tutor/main.js
@@ -0,0 +1,13 @@
+var $character = document.querySelectorAll('.text');
+var indexNumber = 0;
+function typingTutor(event) {
+  if ($character[indexNumber].textContent === event.key) {
+    $character[indexNumber].className = 'text-correct';
+    indexNumber++;
+    $character[indexNumber].classList.add('tracker');
+  } else if ($character[indexNumber].textContent !== event.key) {
+    $character[indexNumber].className = 'text-wrong';
+  }
+}
+
+document.addEventListener('keydown', typingTutor);
