d1d55e1f0e2a5a6b22ed2e616c915c097f530bac
Jonathan Matsuda <jonathanmatsuda3@gmail.com>
2021-10-27 01:02:36 -0700

    In progress

--- string-manipulation-1/src/get-words.js
+++ string-manipulation-1/src/get-words.js
@@ -1,1 +1,28 @@
 /* exported getWords */
+// psuedocode
+// Create storage for the words
+// Identify if there are spaces
+// If there are spaces, take the current word up until that space and place it in storage
+// Otherwise, recognize the space and move onto the next character
+// Return the storage
+
+var wordList = [' '];
+// var word = '';
+function getWords(string) {
+  if (string === []) {
+    return wordList;
+  } else {
+    return string.split(' ');
+  }
+}
+
+//   for (var i = 0; i < string.length; i++) {
+//     if (string[i] !== '') {
+//       word += string[i];
+//     } else {
+//       wordList.push(word);
+//       word = '';
+//     }
+//   }
+//   return wordList;
+// }
--- string-manipulation-1/src/is-vowel.js
+++ string-manipulation-1/src/is-vowel.js
@@ -1,1 +1,10 @@
 /* exported isVowel */
+// pseudocode
+// List out vowels A, E, I, O, U, a, e, i, o, u
+// Create storage for the above list
+// If the provided character includes one of these letters then return true
+// All other letters should return false
+var vowels = ['A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u'];
+function isVowel(char) {
+  return vowels.includes(char);
+}
