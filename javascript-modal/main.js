e847efa455422bd053acc5ab3862563985f9e8f6
Jonathan Matsuda <jonathanmatsuda3@gmail.com>
2021-10-29 21:09:37 -0700

    Scrapped condition statements

--- javascript-modal/main.js
+++ javascript-modal/main.js
@@ -3,26 +3,14 @@ var $openButton = document.querySelector('.open-button');
 var $modalButton = document.querySelector('.modal-button');
 var $dimmerOff = document.querySelector('.dimmer-off');
 function openModal(event) {
-  if ($openButton !== event.target) {
-    $modal.className = 'modal';
-    $openButton.className = 'open-button';
-    $dimmerOff.className = 'dimmer-off';
-  } else {
-    $modal.className = 'modal-open';
-    $openButton.className = 'open-button-dimmer';
-    $dimmerOff.className = 'dimmer-on';
-  }
+  $modal.className = 'modal-open';
+  $openButton.className = 'open-button-dimmer';
+  $dimmerOff.className = 'dimmer-on';
 }
 function closeModal(event) {
-  if ($modalButton !== event.target) {
-    $modal.className = 'modal-open';
-    $openButton.className = 'open-button-dimmer';
-    $dimmerOff.className = 'dimmer-on';
-  } else {
-    $modal.className = 'modal';
-    $openButton.className = 'open-button';
-    $dimmerOff.className = 'dimmer-off';
-  }
+  $modal.className = 'modal';
+  $openButton.className = 'open-button';
+  $dimmerOff.className = 'dimmer-off';
 }
 
 $openButton.addEventListener('click', openModal);
