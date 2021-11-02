e207bd969b542f27ca038393d678c12197a2594a
Jonathan Matsuda <jonathanmatsuda3@gmail.com>
2021-11-01 23:49:20 -0700

    Completed javascript-constructors exercise

--- javascript-constructors/example-constructor.js
+++ javascript-constructors/example-constructor.js
@@ -1,2 +1,9 @@
-// function exampleConstructor() {
-//   console.log(
+function ExampleConstructor() {}
+console.log('value of the prototype property:', ExampleConstructor.prototype);
+console.log('typeof prototype property:', typeof ExampleConstructor.prototype);
+
+var newExampleConstructor = new ExampleConstructor();
+console.log('newExampleConstructor', newExampleConstructor);
+
+var instanceOfExampleConstructor = newExampleConstructor instanceof ExampleConstructor;
+console.log('instanceOfExampleConstructor', instanceOfExampleConstructor);
