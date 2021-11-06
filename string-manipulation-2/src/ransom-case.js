6b26031c6d2dc9d3664b2b8cb024424f4077c505
Jonathan Matsuda <jonathanmatsuda3@gmail.com>
2021-11-06 08:31:51 +0000

    In progress

---
+++ string-manipulation-2/src/ransom-case.js
@@ -0,0 +1,13 @@
+/* exported ransomCase */
+// pseudocode
+
+// function ransomCase(string) {
+//   var newString = '';
+
+// for (var e = 0; e < string.length; e += 2) {
+// var even = word[e].toLowerCase();
+// }
+// for (var o = 1; o < string.length; o += 2) {
+//   var odd = word[o].toUpperCase();
+// }
+// return newString += even + odd;
