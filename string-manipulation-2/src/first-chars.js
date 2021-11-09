5bf5d57cbc147c7473002d3de2decd112abb9ef7
Joe King <joe.king@email.org>
2021-11-09 00:37:21 -0800

    In progress

--- string-manipulation-2/src/first-chars.js
+++ string-manipulation-2/src/first-chars.js
@@ -1,1 +1,11 @@
 /* exported firstChars */
+// psuedocode
+// Create storage for the new chunk of the string we want to extract.
+// Since we are getting the length amount of the first characters begin to extract at position 0.
+// Stop extracting the characters at the length that would be specified by the user.
+// Return the storage created in the first step.
+
+function firstChars(length, string) {
+  var phrase = string.slice(0, length);
+  return phrase;
+}
--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -1,1 +1,16 @@
 /* exported swapChars */
+// pseudocode
+// Create storage for the new swapped string.
+
+function swapChars(firstIndex, secondIndex, string) {
+  var swappedString = '';
+  for (var i = 0; i < string.length; i++) {
+    if (i === firstIndex) {
+      string[i] = string[secondIndex];
+    } else if (i === secondIndex) {
+      string[i] = string[firstIndex];
+    }
+  }
+  swappedString = swappedString + string[i];
+  return swappedString;
+}
