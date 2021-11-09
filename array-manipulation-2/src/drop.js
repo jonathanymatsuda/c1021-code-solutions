5120d7f87526b71848b64f323be3a6c66e59e062
Joe King <joe.king@email.org>
2021-11-09 09:15:45 -0800

    In progress

---
+++ array-manipulation-2/src/drop.js
@@ -0,0 +1,11 @@
+/* exported drop */
+// pseudocode
+// Create new storage for the extracted list of items.
+// Since we want to extract everything from the given 'count' number, start extracting items at this point.
+// Stop extracting through the end of the list. You can get this number by calculating the length of the overall list.
+// Returm the storage created in the first step.
+
+function drop(array, count) {
+  var newArray = array.slice(count, count.length);
+  return newArray;
+}
