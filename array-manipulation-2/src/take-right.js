44ebbfdb7ccdf5458aa051a10e23674ea11e3d4e
Joe King <joe.king@email.org>
2021-11-09 10:22:29 -0800

    In progress

--- array-manipulation-2/src/take-right.js
+++ array-manipulation-2/src/take-right.js
@@ -1,1 +1,12 @@
 /* exported takeRight */
+// psuedocode
+// Create new storage for the array.
+// Since we want to take (keep) everything to the right start by counting from the end of the list. Count as many spaces as the 'count' number given by the user.
+// Then, knowing that we want to extract everything from the right, keep everything through the end of the list. You can calculate the position for this by calculating
+// the entire length of the array.
+// Return the storage created in the first step.
+
+function takeRight(array, count) {
+  var newArray = array.slice(-count, count.length);
+  return newArray;
+}
