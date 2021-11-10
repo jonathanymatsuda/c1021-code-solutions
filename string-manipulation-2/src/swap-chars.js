af1c99227d7792919d7eef66be285658bbd7ede9
Jonathan Matsuda <jonathanmatsuda3@gmail.com>
2021-11-10 09:19:54 -0800

    In progress

--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -3,13 +3,13 @@
 // Create storage for the new swapped string.
 
 function swapChars(firstIndex, secondIndex, string) {
-  var swappedString = '';
-  for (var i = 0; i < string.length; i++) {
+  var split = string.split('');
+  for (var i = 0; i < split.length; i++) {
     if (i === firstIndex) {
-      string[i] = string[secondIndex];
-    } else if (i === secondIndex) {
-      string[i] = string[firstIndex];
+      split[i] = split[secondIndex];
+    }
+    if (i === secondIndex) {
+      split[i] = split[firstIndex];
     }
   }
-  return swappedString.join('');
 }
