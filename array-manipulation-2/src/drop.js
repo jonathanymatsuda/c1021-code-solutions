24a2a067d8b9142c05948a977d1a32af687e7065
Jonathan Matsuda <jonathanmatsuda3@gmail.com>
2021-11-12 02:21:34 -0800

    Completed array-manipulation-2 challenge

--- array-manipulation-2/src/drop.js
+++ array-manipulation-2/src/drop.js
@@ -3,7 +3,7 @@
 // Create new storage for the extracted list of items.
 // Since we want to extract everything from the given 'count' number, start extracting items at this point.
 // Stop extracting through the end of the list. You can get this number by calculating the length of the overall list.
-// Returm the storage created in the first step.
+// Return the storage created in the first step.
 
 function drop(array, count) {
   var newArray = array.slice(count, count.length);
