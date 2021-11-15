e52e73c1b674f21fd2f4da00d2faad3a3880607f
Jonathan Matsuda <jonathanmatsuda3@gmail.com>
2021-11-15 00:57:49 -0800

    In progress

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,1 +1,16 @@
 /* exported defaults */
+// pseudocode
+
+function defaults(target, source) {
+  for (var sKey in source) {
+    var sourceKey = sKey;
+  }
+  for (var tKey in target) {
+    var targetKey = tKey;
+  }
+  if (targetKey === sourceKey) {
+    targetKey = target[targetKey];
+  } else {
+    Object.assign(target, source);
+  }
+}
