209dcadae7282e3991fb0f712aca1d7b8b1b2dfe
Jonathan Matsuda <jonathanmatsuda3@gmail.com>
2021-11-16 10:05:41 -0800

    First exercise in progress

---
+++ string-manipulation-3/src/is-palindromic.js
@@ -0,0 +1,10 @@
+/* exported isPalindromic */
+// pseudocode
+
+// function isPalindromic(string) {
+//   var split = string.split('');
+//   for (var i = split.length; i > 0; i--) {
+//     if(split[i] === string.charAt(0++)
+//   }
+//   return true
+// }
