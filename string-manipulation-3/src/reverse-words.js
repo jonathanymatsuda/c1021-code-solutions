388173126b88a3429ba8abaa5209985f4cc37e6d
Jonathan Matsuda <jonathanmatsuda3@gmail.com>
2021-11-18 08:20:19 +0000

    Last challenge in progress

--- string-manipulation-3/src/reverse-words.js
+++ string-manipulation-3/src/reverse-words.js
@@ -1,11 +1,24 @@
 /* exported reverseWords */
 // psuedocode
+// Create storage for the new string
+// Create storage for a word that needs to be reversed.
+// Starting from the first character of the string, if the current character is not a space add it to the beginnning of a temporary storage for the word.
+// Or else add the current word to the new string with a space in order to separate each reversed word.
+// Reset the storage once the temporary word is added to the new string.
+// Add the final value at the end to ensure the sentence is complete.
+// Return the storage created in the first step.
 
-// function reverseWords(string) {
-//   var output = ' ';
-//   var wordSplit = string.split(' ');
-//   for (var i = wordSplit.length; i >= 0; i--) {
-
-//   }
-//   return output;
-// }
+function reverseWords(string) {
+  var newString = '';
+  var temp = '';
+  for (var i = 0; i < string.length; i++) {
+    if (string[i] !== ' ') {
+      temp = string[i] + temp;
+    } else {
+      newString += temp + ' ';
+      temp = '';
+    }
+  }
+  newString += temp;
+  return newString;
+}
--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,1 +1,22 @@
 /* exported titleCase */
+// pseudocode
+// Create storage for the new string.
+
+function titleCase(string) {
+  var newString = '';
+  var splitWord = string.split(' ');
+  for (var i = 0; i < splitWord.length; i++) {
+    splitWord[i] = splitWord[i].charAt(0).toUpperCase() + splitWord[i].slice(1);
+    if (splitWord[i].length < 4) {
+      splitWord[i] = splitWord[i].toLowerCase();
+    }
+    if (splitWord[i] === 'javascript' || splitWord[i] === 'Javascript') {
+      splitWord[i] = 'JavaScript';
+    }
+    if (splitWord[i] === 'api') {
+      splitWord[i] = 'API';
+    }
+    newString += splitWord[i] + ' ';
+  }
+  return newString.trim();
+}
