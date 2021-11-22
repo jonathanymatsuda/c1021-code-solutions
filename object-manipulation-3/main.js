fcc6ca04a97a9c8cea05ceeeacab41db1fcf9c85
Jonathan Matsuda <jonathanmatsuda3@gmail.com>
2021-11-22 00:40:34 -0800

    In progress

---
+++ object-manipulation-3/main.js
@@ -0,0 +1,51 @@
+console.log('Lodash is loaded:', typeof _ !== 'undefined');
+// pseudocode
+// Create four player objects (playerOne, playerTwo, playerThree, playerFour)
+// 52 Bojects
+
+// var playerOne = {
+//   name: 'Art',
+//   hand:
+// }
+
+// var playerTwo = {
+//   name: 'Judson'
+//   hand:
+// }
+
+// var playerThree = {
+//   name: 'Claire'
+//   hand:
+// }
+
+// var playerFour = {
+//   name: 'Dan'
+//   hand:
+// }
+
+// var rank = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace' ]
+// var suit = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+// var deck = [];
+// var shuffledDeck = [];
+
+// function deckCreation() {
+//   for (var i = 0; i < rank.length; i++) {
+//     for (var j = 0; j < suit.length; j++) {
+//       var card = {Rank: rank[i], Suit: suit[j]}
+//       deck.push(card)
+//     }
+//   }
+//   return deck;
+// }
+
+// function shuffle(deck) {
+//   for (var i = deck.length; i > 0; i--) {
+//     var randomizer = Math.floor(Math.random() * i);
+//     var shuffledCard = deck[randomizer];
+//     shuffledDeck.push(shuffledCard);
+//   }
+//   return shuffledDeck;
+// }
+
+// deckCreation();
+// shuffle(deck);
