2eae6b4177385e08f69519a18361ad81d63c8e9f
Jonathan Matsuda <jonathanmatsuda3@gmail.com>
2021-11-23 04:29:45 +0000

    Used Math.min() to find shortest array thanks!

--- array-manipulation-3/src/zip.js
+++ array-manipulation-3/src/zip.js
@@ -1,9 +1,8 @@
 /* exported zip */
 // pseudocode
 // Create storage for the new array.
-// The tests only account for arrays with the least amount of values meaning that any extra value from either array without a matching pair can be eliminated.
-// Eliminate the extra value from the first array if it is longer than the second array.
-// Eliminare the exrra value from the second array if it is longer than the first array.
+// The tests only account for arrays with the least amount of values.
+// Take the array with the shortest length and be sure to only push values for both arrays up to that length.
 // Now that they are the same length, extract a value from the first array.
 // Extract a value from the second array in that same position.
 // Push it to the storage created in the first step.
@@ -12,13 +11,8 @@
 
 function zip(first, second) {
   var newArray = [];
-  if (first.length > second.length) {
-    first.splice(first.length - 1, 1);
-  }
-  if (second.length > first.length) {
-    second.splice(second.length - 1, 1);
-  }
-  for (var i = 0; i < first.length; i++) {
+  var length = Math.min(first.length, second.length);
+  for (var i = 0; i < length; i++) {
     newArray.push([first[i], second[i]]);
   }
   return newArray;
