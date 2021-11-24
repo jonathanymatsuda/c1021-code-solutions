93fc4a7fde5104d4af613b10127c96aa651601d8
Jonathan Matsuda <jonathanmatsuda3@gmail.com>
2021-11-24 00:22:35 +0000

    In progress

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -1,28 +1,24 @@
 console.log('Lodash is loaded:', typeof _ !== 'undefined');
 // pseudocode
-// Create four player objects (playerOne, playerTwo, playerThree, playerFour)
-// 52 card objects
+// Create four player objects in an array.
+// 52 card objects:
+// Create an array with all the ranks possible for the cards.
+// Create an array with all suits possible for the cards.
+// Create storage for the deck of 52 card objects.
+// In a function create the deck with 52 cards:
+// Extract each possible rank suit combination by first going into the rank array and then going into the suit array.
+// Take each possible pair and push it to the storage for deck. Your result should be an array of 52 elements.
+// Return the deck - (may not need this later on)
+// Shuffle deck:
+// High level overview: In order to shuffle the deck we essentially want to take the first card in the created deck and replace it with a random
+// position
+// Within the deck, create temmporary storage for the card that will appear on top (all cards will essentially appear on the top)
+// and will get replaced because you have to loop)
+// Create a randomizer that will generate a number up to 52
+// Replace the card that is on the top position of the deck with whatever position the random number generator generates.
+// Set the temporary storage to the storage
 
-// var playerOne = {
-//   name: 'Art',
-//   hand:
-// }
-
-// var playerTwo = {
-//   name: 'Judson'
-//   hand:
-// }
-
-// var playerThree = {
-//   name: 'Claire'
-//   hand:
-// }
-
-// var playerFour = {
-//   name: 'Dan'
-//   hand:
-// }
-
+// var players = [{name:'Art', hand:}, {name:'Judson', hand:}, {name:'Claire', hand:}, {name:'Dan', hand:}]
 // var rank = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace' ]
 // var suit = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
 // var deck = [];
