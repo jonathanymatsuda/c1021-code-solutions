f4b9edab3b09ae9b51b448a9dbf017f34e05aee4
Jonathan Matsuda <jonathanmatsuda3@gmail.com>
2021-12-02 08:16:35 +0000

    Post in progress

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -10,7 +10,16 @@ app.get('/api/notes', (req, res) => {
   res.status(200).json(notesArray);
 });
 
-app.get();
+app.get('/api/notes/:id', (req, res) => {
+  const id = req.params.id;
+  if (id <= 0) {
+    res.status(400).json({ error: 'id must be a positive integer' });
+  } else if (dataObject.notes[id] !== undefined) {
+    res.status(200).json(dataObject.notes[id]);
+  } else if (dataObject.notes[id] === undefined) {
+    res.status(404).json({ error: 'cannot find note with id ' + id });
+  }
+});
 
 app.listen(3000, () => {
   // console.log('Listening to port 3000');
