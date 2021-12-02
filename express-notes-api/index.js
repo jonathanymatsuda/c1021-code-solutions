1d5df4b9abab35c5c480c380e53cc89f2f24a68e
Jonathan Matsuda <jonathanmatsuda3@gmail.com>
2021-12-02 22:31:26 +0000

    On Put request

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const dataObject = require('./data.json');
+const fs = require('fs');
 
 app.get('/api/notes', (req, res) => {
   const notesArray = [];
@@ -12,13 +13,76 @@ app.get('/api/notes', (req, res) => {
 
 app.get('/api/notes/:id', (req, res) => {
   const id = req.params.id;
-  if (id <= 0) {
+  if (Number.isInteger(id) && id <= 0) {
     res.status(400).json({ error: 'id must be a positive integer' });
   } else if (dataObject.notes[id] !== undefined) {
     res.status(200).json(dataObject.notes[id]);
   } else if (dataObject.notes[id] === undefined) {
     res.status(404).json({ error: 'cannot find note with id ' + id });
   }
+});
+
+const jsonMiddleware = express.json();
+app.use('/api/notes', jsonMiddleware);
+
+app.post('/api/notes', (req, res) => {
+  const newNote = req.body;
+  if (newNote.content === undefined) {
+    res.status(400).json({ error: 'content is a required field' });
+  } else {
+    newNote.id = dataObject.nextId;
+    dataObject.notes[dataObject.nextId] = newNote;
+    dataObject.nextId++;
+    fs.writeFile('./data.json', JSON.stringify(dataObject, null, 2), err => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ error: 'An unexpected error occurred.' });
+      } else {
+        res.status(201).json(newNote);
+      }
+    });
+  }
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  const id = req.params.id;
+  if (!Number.isInteger(id) || id <= 0) {
+    res.status(400).json({ error: 'id must be a positive integer' });
+  } else if (dataObject.notes[id] === undefined) {
+    res.status(404).json({ error: 'cannot find note with id ' + id });
+  } else if (dataObject.notes[id] !== undefined) {
+    delete dataObject.notes[id];
+    fs.writeFile('./data.json', JSON.stringify(dataObject, null, 2), err => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ error: 'An unexpected error occurred.' });
+      } else {
+        res.sendStatus(204);
+      }
+    });
+  }
+});
+
+app.put('/api/notes/:id', (req, res) => {
+  const updatedNote = req.body;
+  const id = req.params.id;
+  if (!Number.isInteger(id) || id <= 0) {
+    res.status(400).json({ error: 'id must be a positive integer' });
+  } else if (updatedNote.content === undefined) {
+    res.status(400).json({ error: 'content is a required field ' });
+  } else if (updatedNote.content !== undefined && dataObject.notes[id] === undefined) {
+    res.status(404).json({ error: 'cannot find note with id ' + id });
+  } else if (updatedNote.content !== undefined && dataObject.notes[id] !== undefined) {
+    dataObject.notes[id] = updatedNote;
+    dataObject.notes[id].id = updatedNote.id;
+  } fs.writeFile('./data.json', JSON.stringify(dataObject, null, 2), err => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'An unexpected error occurred.' });
+    } else {
+      res.status(200).json(updatedNote[id]);
+    }
+  });
 });
 
 app.listen(3000, () => {
