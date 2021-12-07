625d3423fe01ff988491faf0dc8d501f38fbe330
Jonathan Matsuda <jonathanmatsuda3@gmail.com>
2021-12-07 08:36:54 +0000

    In progress

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -28,6 +28,29 @@ app.get('/api/grades', (req, res, next) => {
     });
 });
 
+app.post('/api/grades', (req, res, next) => {
+  const newGrade = req.body;
+  const grade = Number(req.body.score);
+  if (!Number.isInteger(grade) || grade < 0 || grade > 100) {
+    res.status(400).json({ error: 'score must be valid (0-100)' });
+  } else if (newGrade.name === undefined || newGrade.course === undefined || newGrade.score === undefined) {
+    res.status(400).json({ error: 'Must be a valid input' });
+  } else {
+    const sql = `
+    insert into "grades" ("name", "course", "score")
+    values ('newGrade.name', 'newGrade.course', 'newGrade.score')
+  `;
+    db.query(sql)
+      .then(result => {
+        res.status(201).json({ newGrade });
+      })
+      .catch(error => {
+        console.error(error);
+        res.status(500).json({ error: 'An unexpected error occurred' });
+      });
+  }
+});
+
 app.listen(3000, () => {
   console.log('Listening on port 3000'); //eslint-disable-line
 });
