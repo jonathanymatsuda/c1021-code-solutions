eba0bd908946c55c754b184369ae68a3b6a2295a
Jonathan Matsuda <jonathanmatsuda3@gmail.com>
2021-12-07 20:54:06 +0000

    Put in progress

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -7,8 +7,6 @@ const db = new pg.Pool({
     rejectUnauthorized: false
   }
 });
-const jsonMiddleware = express.json();
-app.use('/api/grades', jsonMiddleware);
 
 app.get('/api/grades', (req, res, next) => {
   const grades = [];
@@ -28,28 +26,53 @@ app.get('/api/grades', (req, res, next) => {
     });
 });
 
+const jsonMiddleware = express.json();
+app.use('/api/grades', jsonMiddleware);
+
 app.post('/api/grades', (req, res, next) => {
   const newGrade = req.body;
-  const grade = Number(req.body.score);
+  const grade = Number(newGrade.score);
   if (!Number.isInteger(grade) || grade < 0 || grade > 100) {
     res.status(400).json({ error: 'score must be valid (0-100)' });
+    return;
   } else if (newGrade.name === undefined || newGrade.course === undefined || newGrade.score === undefined) {
     res.status(400).json({ error: 'Must be a valid input' });
-  } else {
-    const sql = `
-    insert into "grades" ("name", "course", "score")
-    values ('newGrade.name', 'newGrade.course', 'newGrade.score')
-  `;
-    db.query(sql)
-      .then(result => {
-        res.status(201).json({ newGrade });
-      })
-      .catch(error => {
-        console.error(error);
-        res.status(500).json({ error: 'An unexpected error occurred' });
-      });
+    return;
   }
+  const values = [newGrade.name, newGrade.course, newGrade.score];
+  const sql = `
+    insert into "grades" ("name", "course", "score")
+    values ($1, $2, $3)
+    returning *
+  `;
+  db.query(sql, values)
+    .then(result => {
+      res.status(201).json(result.rows);
+    })
+    .catch(error => {
+      console.error(error);
+      res.status(500).json({ error: 'An unexpected error occurred' });
+    });
+
 });
+
+// app.put('/api/grades/:gradeId', (req, res, next) => {
+//   const gradeId = Number(req.params.gradeId);
+//   const newGrade = req.body;
+//   const grade = Number(newGrade.score);
+//   if (!Number.isInteger(gradeId) || gradeId <= 0) {
+//     res.status(400).json({ error: 'gradeId must be a positive integer' });
+//   } else if (!Number.isInteger(grade) || grade < 0 || grade > 100) {
+//     res.status(400).json({ error: 'score must be valid (0-100)' });
+//   } else if (newGrade.name === undefined || newGrade.course === undefined || newGrade.score === undefined) {
+//     res.status(400).json({ error: 'Must be a valid input' });
+//   }
+//   const values = [newGrade.name, newGrade.course, newGrade.score];
+//   const sql = `
+//     update "grades"
+//     set "grades" =
+//   `
+// );
 
 app.listen(3000, () => {
   console.log('Listening on port 3000'); //eslint-disable-line
