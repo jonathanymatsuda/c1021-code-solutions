7157f78b3a5e07b1bbf214da012329ad63b43526
Jonathan Matsuda <jonathanmatsuda3@gmail.com>
2021-12-09 05:36:19 +0000

    Completed react-elements exercise

---
+++ react-elements/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const hello = React.createElement('h1', null, 'Hello, React!');
+ReactDOM.render(hello, document.getElementById('root'));
