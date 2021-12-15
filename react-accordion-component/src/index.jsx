48ee35c451961f518ec98879977fd18ca9586877
Jonathan Matsuda <jonathanmatsuda3@gmail.com>
2021-12-15 02:01:53 +0000

    In progress

---
+++ react-accordion-component/src/index.jsx
@@ -0,0 +1,8 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+// const languages = [
+//   { name: 'Hypertext Markup Language', description: 'HTML is the standard markup language for creating web pages and web applications. With Cascading Style Sheets (CSS) and JavaScript, it forms a triad of cornerstone technologies for the web.' },
+//   { name: 'Cascading Style Sheets', description: 'CSS is a style sheet langauge used for describing the presentation of a document written in a markup language like HTML. CSS is a cornerston technology of the World Wide web alongside HTML and Javascript.' },
+//   { name: 'JavaScript', description: 'JS is a high-level, interpreted programming language that conforms to the ECMAScript specification. JavaScript has curly-bracket syntax, dynamic typing, prototype-based object-orientation, and first-class functions.' }
+// ];
