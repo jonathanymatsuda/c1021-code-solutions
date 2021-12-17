334c0090470c4a375531628629a70f0e9bc63c63
Jonathan Matsuda <jonathanmatsuda3@gmail.com>
2021-12-17 17:25:39 +0000

    Close to fixing bug

--- fetch-in-react-practice/client/components/app.jsx
+++ fetch-in-react-practice/client/components/app.jsx
@@ -66,7 +66,7 @@ export default class App extends React.Component {
       .then(todoUpdated => {
         const copy = this.state.todos.slice();
         todoUpdated = copy[todoIndex];
-        this.setState({ todos: todoUpdated });
+        this.setState({ todos: copy });
       })
       .catch(err => console.error(err));
   }
