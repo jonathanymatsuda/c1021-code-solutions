5883006efef4b8a6a6a8030d885cecd606bc66b7
Jonathan Matsuda <jonathanmatsuda3@gmail.com>
2021-12-20 07:01:38 +0000

    In progress

--- react-accordion-component/src/index.jsx
+++ react-accordion-component/src/index.jsx
@@ -1,5 +1,5 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 // const languageInfo = [
 //   { name: 'Hypertext Markup Language', description: 'HTML is the standard markup language for creating web pages and web applications. With Cascading Style Sheets (CSS) and JavaScript, it forms a triad of cornerstone technologies for the web.' },
@@ -7,37 +7,42 @@
 //   { name: 'JavaScript', description: 'JS is a high-level, interpreted programming language that conforms to the ECMAScript specification. JavaScript has curly-bracket syntax, dynamic typing, prototype-based object-orientation, and first-class functions.' }
 // ];
 
-// class Accordion extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       isClicked: false
-//     };
-//   }
+class Accordion extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isClicked: false
+    };
+  }
 
-//   handleClick(event) {
-//   }
+  render() {
+    return <p>Test</p>;
+  //   const language = languageInfo.map(selection =>
+  //     <React.Fragment key={languageInfo.name}>
+  //       <h3>{languageInfo.name}</h3>
+  //       <p>{languageInfo.description}</p>
+  //     </React.Fragment>
+  //   );
+  //   return (
+  //     <div className='accordion'>
+  //       {language}
+  //     </div>
+  //   );
+  // }
+  }
+}
 
-//   render() {
-//     const language = languageInfo.map(selection =>
-//       <li key={selection.name}>{selection.description}</li>
-//     );
-//     return (
-//       <div className='accordion'>
-//       </div>
-//     );
-//   }
-// }
-
-// ReactDOM.render(<Accordion/>, document.querySelector('#root'));
+ReactDOM.render(<Accordion/>, document.querySelector('#root'));
 
 /* <div class="accordion">
-  <ul>
-    <h3>Hypertext Markup Language</h3>
-    <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, dignissimos in reiciendis enim expedita accusamus aspernatur incidunt iure quas maiores necessitatibus a? Quaerat cumque ex voluptates porro impedit libero in!</li>
-    <h3>Cascading Style Sheets</h3>
-    <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, officia nam! Ut alias cupiditate quisquam illum cum voluptatibus facere sapiente sequi nemo rerum eaque, natus soluta dolores, mollitia aut ea.</li>
-    <h3>JavaScript</h3>
-    <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse minus, nisi illum temporibus fuga iure labore minima asperiores exercitationem, autem earum error obcaecati repellendus numquam maxime doloremque hic illo corrupti?</li>
-  </ul>
+  <h3>Hypertext Markup Language</h3>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, dignissimos in reiciendis enim expedita accusamus
+    aspernatur incidunt iure quas maiores necessitatibus a? Quaerat cumque ex voluptates porro impedit libero in!</p>
+  <h3>Cascading Style Sheets</h3>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, officia nam! Ut alias cupiditate quisquam illum
+    cum voluptatibus facere sapiente sequi nemo rerum eaque, natus soluta dolores, mollitia aut ea.</p>
+  <h3>JavaScript</h3>
+  <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse minus, nisi illum temporibus fuga iure labore
+    minima asperiores exercitationem, autem earum error obcaecati repellendus numquam maxime doloremque hic illo
+    corrupti?</p>
 </div> */
