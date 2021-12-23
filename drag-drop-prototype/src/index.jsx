d87d091fd2f3fff335a661191026d2b658f96433
Jonathan Matsuda <jonathanmatsuda3@gmail.com>
2021-12-23 09:02:03 +0000

    Working on onDragEnd prototype

--- drag-drop-prototype/src/index.jsx
+++ drag-drop-prototype/src/index.jsx
@@ -60,9 +60,31 @@ class DragApp extends React.Component {
     };
   }
 
-  // onDragEnd() {
+  onDragEnd(result) {
+    const { destination, source, draggableId } = result;
+    if (!destination) {
+      return;
+    }
+    if (destination.droppableId === source.droppableId && destination.index === source.index) {
+      return;
+    }
+    const column = this.state.kanbanList[source.droppableId];
+    const newTaskIds = Array.from(column.newTaskIds);
+    newTaskIds.splice(source.index, 1);
+    newTaskIds.splice(destination.index, 0, draggableId);
 
-  // }
+    const newColumn = {
+      ...column,
+      taskIds: newTaskIds
+    };
+
+    const newState = {
+      ...this.state,
+      ...this.state.columns,
+      [newColumn.id]: newColumn
+    };
+    this.setState({ newState });
+  }
 
   render() {
     const kanRender = kanbanList.map(column =>
