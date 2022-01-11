abbab39b3fdef6937b9fbe3ae7e2a02b8ffe3a91
Jonathan Matsuda <jonathanmatsuda3@gmail.com>
2022-01-11 20:40:42 +0000

    Cleaned up a bit more after understanding

--- big-o-notation/big-o.js
+++ big-o-notation/big-o.js
@@ -5,11 +5,11 @@ function uniqueLinear(words) {
   const unique = [];                // 1 * 1 = O(1)
   for (
     let i = 0;                      // 1 * 1 = O(1)
-    i < words.length;               // 2 * n = O(2n) ~= 0(n)
-    i++                             // 2 * n = O(2n) ~= 0(n)
+    i < words.length;               // 2 * n = 0(n)
+    i++                             // 2 * n = 0(n)
   ) {
-    const word = words[i];          // 2 * n = O(2n) ~= 0(n)
-    if (!seen[word]) {              // 5 * n = O(1)
+    const word = words[i];          // 2 * n = 0(n)
+    if (!seen[word]) {              // 5 * n = O(n)
       seen[word] = true;
       unique[unique.length] = word; // 3 * n = O(n)
     }
@@ -21,11 +21,11 @@ function uniqueQuadratic(words) {
   const unique = [];                // 1 * 1 = O(1)
   for (
     let i = 0;                      // 1 * 1 = O(1)
-    i < words.length;               // 2 * n = O(2n) ~= 0(n)
-    i++                             // 2 * n = O(2n) ~= 0(n)
+    i < words.length;               // 2 * n = 0(n)
+    i++                             // 2 * n = 0(n)
   ) {
-    const word = words[i];          // 3 * n = O(3n) ~= 0(n)
-    let isUnique = true;            // 1 * 1 = O(1)
+    const word = words[i];          // 3 * n = O(n)
+    let isUnique = true;            // 1 * n = O(n)
     for (
       let c = 0;                    // 1 * n = O(n)
       c < i;                        // 2 * n^2 = O(n^2)
