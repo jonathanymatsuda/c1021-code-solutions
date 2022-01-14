0b1a03689dd532c7110da8f2ede4b1f9a23ab4c3
Jonathan Matsuda <jonathanmatsuda3@gmail.com>
2022-01-14 06:52:19 +0000

    Completed data-structures-linked-lists exercise

--- data-structures-linked-lists/includes.js
+++ data-structures-linked-lists/includes.js
@@ -1,10 +1,11 @@
 /* exported includes */
 
 function includes(list, value) {
-  while (list !== null) {
-    list = list.next;
+  while (list) {
     if (list.data === value) {
       return true;
     }
+    list = list.next;
   }
+  return false;
 }
