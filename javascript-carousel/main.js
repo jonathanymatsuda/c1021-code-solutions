9de18cd7a39b2c67514eb0158ea1c27e8f2cacb6
Jonathan Matsuda <jonathanmatsuda3@gmail.com>
2022-01-18 05:13:31 +0000

    Challenge in progress

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -1,20 +1,28 @@
-// var $pokemon = document.querySelector('.pokemon');
-var $arrowLeft = document.querySelector('.fa-chevron-circle-left');
-var $arrowRight = document.querySelector('.fa-chevron-circle-right');
-// var $faCircle = document.querySelector('.fa-circle');
-var $pokemonImage = document.querySelector('.pokemon-image');
-var pokemonImages = ['images/025.png', 'images/001.png', 'images/004.png', 'images/007.png', 'images/039.png'];
-// var carouselLoop = null;
-// function pokemonLoop() {
-//   for (var imageChanger = 1; )
-// }
+// const $pokemon = document.querySelector('.pokemon');
+const $arrowLeft = document.querySelector('.fa-chevron-circle-left');
+const $arrowRight = document.querySelector('.fa-chevron-circle-right');
+// const $faCircle = document.querySelector('.fa-circle');
+const $pokemonImage = document.querySelector('.pokemon-image');
 
-function viewSwapping(event) {
-  for (var swap = 0; swap < pokemonImages.length; swap++) {
-    if ($arrowLeft === event.target || $arrowRight === event.target) {
-      $pokemonImage.src = pokemonImages[swap];
-    }
+var imagePosition = 0;
+var imageArray = ['images/025.png', 'images/001.png', 'images/004.png', 'images/007.png', 'images/039.png'];
+
+const imageForward = () => {
+  imagePosition++;
+  $pokemonImage.setAttribute('src', imageArray[imagePosition]);
+  if (imagePosition > imageArray.length) {
+    imagePosition = 0;
   }
-}
+};
 
-$arrowRight.addEventListener('click', viewSwapping);
+$arrowRight.addEventListener('click', imageForward);
+
+const imageBackward = () => {
+  imagePosition--;
+  $pokemonImage.setAttribute('src', imageArray[imagePosition]);
+  if (imagePosition < 0) {
+    imagePosition = 0;
+  }
+};
+
+$arrowLeft.addEventListener('click', imageBackward);
