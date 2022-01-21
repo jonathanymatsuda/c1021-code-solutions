abf5617886627f84203dbad2bed73ee5b05e4e27
Jonathan Matsuda <jonathanmatsuda3@gmail.com>
2022-01-21 06:44:12 +0000

    Completed react-carousel-component challenge

--- react-carousel-component/src/carousel.jsx
+++ react-carousel-component/src/carousel.jsx
@@ -7,14 +7,25 @@ export default class Carousel extends React.Component {
       currentImagePosition: 0
     };
     this.handleArrowClick = this.handleArrowClick.bind(this);
+    this.handleCircleClick = this.handleCircleClick.bind(this);
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      if (this.state.currentImagePosition >= this.props.content.length - 1) {
+        this.setState({ currentImagePosition: 0 });
+      } else {
+        this.setState({ currentImagePosition: this.state.currentImagePosition + 1 });
+      }
+    }, 3000);
   }
 
   handleArrowClick(event) {
+    this.setState({ setIntervalStatus: !this.state.setIntervalStatus });
     if (event.target.className === 'fas fa-chevron-circle-right fa-2x') {
       this.setState({ currentImagePosition: this.state.currentImagePosition + 1 });
       if (this.state.currentImagePosition >= this.props.content.length - 1) {
         this.setState({ currentImagePosition: 0 });
-
       }
     }
     if (event.target.className === 'fas fa-chevron-circle-left fa-2x') {
@@ -25,7 +36,15 @@ export default class Carousel extends React.Component {
     }
   }
 
+  handleCircleClick(event) {
+    const targetedCircle = Number(event.target.id);
+    if (event.target.className === 'far fa-circle ml-10') {
+      this.setState({ currentImagePosition: targetedCircle });
+    }
+  }
+
   render() {
+    const position = this.state.currentImagePosition;
     return (
       <>
         <header>
@@ -33,40 +52,21 @@ export default class Carousel extends React.Component {
             <h1>LFZ Smash Carousel</h1>
           </div>
         </header>
-        <div key={this.props.content[this.state.currentImagePosition].id} className="row align-items-center space">
+        <div className="row align-items-center space">
           <i className="fas fa-chevron-circle-left fa-2x" onClick={this.handleArrowClick}></i>
-          <img src={this.props.content[this.state.currentImagePosition].image} alt="Smash?!"></img>
+          <img src={this.props.content[position].image} alt="Smash?!"></img>
           <i className="fas fa-chevron-circle-right fa-2x" onClick={this.handleArrowClick}></i>
         </div>
         <div className="row text-align-center mt-70">
-          <div className="circle-container">
-            <i circleid="0" className="fas fa-circle ml-10"></i>
-            <i circleid="1" className="far fa-circle ml-10"></i>
-            <i circleid="2" className="far fa-circle ml-10"></i>
-            <i circleid="3" className="far fa-circle ml-10"></i>
-            <i circleid="4" className="far fa-circle ml-10"></i>
-          </div>
+         {this.props.content.map((buttonDistributed, index) => (
+           <i className={position === index
+             ? 'fas fa-circle ml-10'
+             : 'far fa-circle ml-10'}
+             key={buttonDistributed.id} onClick={this.handleCircleClick} id={index}></i>
+         )
+         )}
         </div>
       </>
     );
   }
 }
-//  <header>
-//    <div class="row text-align-center">
-//      <h1>LFZ Smash Carousel</h1>
-//    </div>
-//  </header>
-//  <div class="row align-items-center space">
-//    <i class="fas fa-chevron-circle-left fa-2x"></i>
-//    <img class="character-image" src="images/image001.png" alt="Smash?!">
-//    <i class="fas fa-chevron-circle-right fa-2x"></i>
-//  </div>
-// //  <div class="row text-align-center mt-70">
-// //    <div class="circle-container">
-// //      <i circleid="0" class="fas fa-circle ml-10"></i>
-// //      <i circleid="1" class="far fa-circle ml-10"></i>
-// //      <i circleid="2" class="far fa-circle ml-10"></i>
-// //      <i circleid="3" class="far fa-circle ml-10"></i>
-// //      <i circleid="4" class="far fa-circle ml-10"></i>
-// //    </div>
-// //  </div>
